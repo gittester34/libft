90a8e54eb655a2aee53890bc197b490dcb937b76
tifaout <tifaout@DESKTOP-6EV8A88.localdomain>
2026-09-22 11:07:28 +0100

    test

---
+++ ft_strncpy.c
@@ -0,0 +1,11 @@
+char    *ft_strncpy(char *dest, char *src, int n )
+{
+     int i = 0;
+     while (src[i] != '\0' && i < n)
+     {
+        dest[i] = src[i];
+        i++;
+     }
+     dest[i] = '\0';
+     return (dest);
+}
